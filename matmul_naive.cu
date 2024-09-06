9e53bb8eb9b2b197bdb172dbc7e5ddf3af31c6b4
PAILLE Leo <leo.paille@enseirb-matmeca.fr>
2024-09-06 18:09:26 +0200

    fix a & b generatio

--- matmul_naive.cu
+++ matmul_naive.cu
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
+#include <assert.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
 
 uint64_t nanos()
 {
@@ -9,8 +14,8 @@ uint64_t nanos()
     return (uint64_t)start.tv_sec * 1000000000 + (uint64_t)start.tv_nsec;
 }
 
-#define N 4096
-#define CEIL_DIV(a,b) ((a+b-1)/b)
+#define N 1024
+#define CEIL_DIV(a, b) ((a + b - 1) / b)
 
 __global__ void matmul(float *a, float *b, float *c, int n)
 {
@@ -25,14 +30,19 @@ __global__ void matmul(float *a, float *b, float *c, int n)
     float acc = 0.0f;
     for (int k = 0; k < n; k++)
     {
+        assert((thread_id_y * n + k) < (n*n));  // check out of bounds
+        assert((k * n + thread_id_x) < (n*n));  // check out of bounds
         acc += a[thread_id_y * n + k] * b[k * n + thread_id_x];
     }
 
+    assert((thread_id_y * n + thread_id_x) < (n*n));  // check out of bounds
     c[thread_id_y * n + thread_id_x] = acc;
 }
 
 int main()
 {
+     srand(time(NULL));
+
     float *a = (float *)malloc(N * N * sizeof(float));
     float *b = (float *)malloc(N * N * sizeof(float));
     float *c = (float *)malloc(N * N * sizeof(float));
@@ -45,8 +55,8 @@ int main()
     // fill a & b and zero out c
     for (int i = 0; i < (N * N); i++)
     {
-        a[i] = 1.0f;
-        b[i] = 2.0f;
+        a[i] = ((double)rand()) / INT_MAX;
+        b[i] = ((double)rand()) / INT_MAX;
         c[i] = 0.0f;
     }
 
@@ -69,41 +79,39 @@ int main()
     printf("%f GFLOP/S -- %.2f ms\n", gflop / s, s * 1e3);
 
     {
-	// // compute naive reference matmul on cpu
-    // printf("Computing reference matmul result on cpu\n");
-    // float *reference_c = (float *)malloc(N * N * sizeof(float));
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //     {
-    //         float acc = 0.0f;
-    //         for (int k = 0; k < N; k++)
-    //         {
-    //             acc += a[i * N + k] * b[k * N + j];
-    //         }
-    //         reference_c[i * N + j] = acc;
-    //     }
-    // }
+        // compute naive reference matmul on cpu
+        printf("Computing reference matmul result on cpu\n");
+        float *reference_c = (float *)malloc(N * N * sizeof(float));
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                float acc = 0.0f;
+                for (int k = 0; k < N; k++)
+                {
+                    acc += a[i * N + k] * b[k * N + j];
+                }
+                reference_c[i * N + j] = acc;
+            }
+        }
 
-    // // check each item
-    // printf("Comparing reference result with gpu result\n");
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //     {
-    //         if (c[i * N + j] != reference_c[i * N + j])
-    //         {
-    //             printf("ERROR at i=%d j=%d (should be %f, is %f)\n", i, j, reference_c[i * N + j], c[i * N + j]);
-    //             exit(1);
-    //         }
-    //     }
-    // }
-    // free(reference_c);
-    // printf("ALL GOOD\n");
-
-
+        // check each item
+        printf("Comparing reference result with gpu result\n");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (reference_c[i * N + j] - c[i * N + j] > 1e-3)
+                {
+                    printf("ERROR at i=%d j=%d (should be %f, is %f)\n", i, j, reference_c[i * N + j], c[i * N + j]);
+                    exit(1);
+                }
+            }
+        }
+        free(reference_c);
+        printf("ALL GOOD\n");
     }
-    
+
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
